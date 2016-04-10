706c511d20ab8f2eb559d898442822e9b4981ff7
Sahil Gupta <emailsahilgupta12@gmail.com>
2016-04-10 17:43:47 +0530

    Adds logic to the main view controller to initiate querying once user hits search

--- Revirant/ViewController.m
+++ Revirant/ViewController.m
@@ -8,40 +8,75 @@
 
 #import "ViewController.h"
 #import "RRUtilities.h"
+#import "YelpAPINetworkClient.h"
 
-@interface ViewController () <CLLocationManagerDelegate>
-@property (nonatomic, strong) CLLocationManager *locationManager;
+@interface ViewController () <CLLocationManagerDelegate, UISearchBarDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong,nonatomic) NSString *queryString;
+@property (strong, nonatomic) CLLocation *currentLocation;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
- 
+    
+    self.searchBar.delegate = self;
+    
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    
+}
+
+#pragma mark - Search Bar
+-(void) searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [self.searchBar resignFirstResponder];
+    self.queryString = self.searchBar.text;
+    [self determineUserLocationAndFindBusinesses];
+}
+
+#pragma mark - Location Manager
+
+-(void) determineUserLocationAndFindBusinesses {
     if([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
         [self.locationManager requestWhenInUseAuthorization];
     }
-    
     [self.locationManager startUpdatingLocation];
 }
+
 
 #pragma mark - CLLocationManagerDelegate
 
 -(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-    UIAlertController *errorController = [RRUtilities alertWithTitle:@"Couldn't access location" message:@"Revirant doesn't have access to your location. Your location will be automatically set to our default location - Toronto, CA" okayHandler:nil];
+    UIAlertController *errorController = [RRUtilities alertWithTitle:@"Couldn't access location" message:@"Revirant doesn't have access to your location. Your location will be automatically set to Toronto, CA. Please enable access to your location in Settings." okayHandler:^{
+        [self fetchRestaurantsWithTerm:self.queryString location:nil];
+    }];
     [self presentViewController:errorController animated:YES completion:nil];
 }
 
 -(void) locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations {
-    if ([locations lastObject] != nil) {
-        CLLocation *location = [locations lastObject];
-        NSLog(@"Latitude: %f, Logitude: %f", location.coordinate.latitude, location.coordinate.longitude);
-        [self.locationManager stopUpdatingLocation];
+    [self.locationManager stopUpdatingLocation];
+    
+    if ([locations lastObject] != nil && self.currentLocation == nil) {
+        self.currentLocation = [locations lastObject];
+        NSLog(@"%@",self.currentLocation);
+        [self fetchRestaurantsWithTerm:self.queryString location:self.currentLocation];
     }
+}
+
+#pragma mark - Fetch Restaurants
+
+-(void) fetchRestaurantsWithTerm:(NSString *)term location:(CLLocation *)location {
+    YelpAPINetworkClient *client = [[YelpAPINetworkClient alloc] init];
+    [client queryBusinessesWithTerm:term location:location
+                        withSuccess:^(NSArray *businesses) {
+                            NSLog(@"%@", businesses);
+                        } error:^(NSError *error) {
+                            NSLog(@"%@",error);
+                        } always:^{
+                            self.currentLocation = nil;
+                        } session:[NSURLSession sharedSession]];
 }
 
 @end
